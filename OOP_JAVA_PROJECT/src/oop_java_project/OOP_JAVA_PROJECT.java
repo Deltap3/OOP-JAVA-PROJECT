e143434d0e1c0ff2d99bdfc23a58b2d7951ba6a4
Pierre <maisterrenap@gmail.com>
2020-11-14 13:50:00 +0100

    ajouter des élements et changement tables

--- OOP_JAVA_PROJECT/src/oop_java_project/OOP_JAVA_PROJECT.java
+++ OOP_JAVA_PROJECT/src/oop_java_project/OOP_JAVA_PROJECT.java
@@ -5,7 +5,9 @@
  */
 package oop_java_project;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,9 +23,6 @@ public class OOP_JAVA_PROJECT {
     public static void main(String[] args) {
         try { 
             Connection dataBase = new Connection("project", "root", "password");
-            dataBase.personExist("Pierre33", "Pierrot");
-            dataBase.personExist("Pierre31", "Pierrot");
-            dataBase.personExist("Pierre33", "Pierro");
         } catch (SQLException ex) {
             Logger.getLogger(OOP_JAVA_PROJECT.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
