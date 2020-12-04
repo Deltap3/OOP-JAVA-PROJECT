9a7409657155e145a64e3410dc943380a5e7139d
JDECE <61450846+JDECE@users.noreply.github.com>
2020-12-04 22:31:10 +0100

    modif s

--- OOP_JAVA_PROJECT/src/view/StatPanel.java
+++ OOP_JAVA_PROJECT/src/view/StatPanel.java
@@ -7,6 +7,7 @@ package view;
 
 import controller.ChangePanelListener;
 import controller.StatListener;
+import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
@@ -21,13 +22,14 @@ public class StatPanel extends JPanel{
     public StatPanel(MainFrame frame, String statType)
     {
         super();
+        this.setLayout(new BorderLayout());
         JLabel label= new JLabel(statType);
-        this.add(label);
+        this.add(label, BorderLayout.NORTH);
         Statistics statFrame= new Statistics(statType);
-        this.add(statFrame);
+        this.add(statFrame, BorderLayout.CENTER);
         JButton btn= new JButton("ok");
         btn.addActionListener(new StatListener(frame));
-        this.add(btn);
+        this.add(btn, BorderLayout.SOUTH);
         
         statFrame.setVisible(true);
         
