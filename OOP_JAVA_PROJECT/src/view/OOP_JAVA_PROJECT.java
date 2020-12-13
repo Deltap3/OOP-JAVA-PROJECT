db0db33cb772ee33987d230c644572508d436569
Pierre <maisterrenap@gmail.com>
2020-12-13 18:40:20 +0100

    commentaires du main maj

--- OOP_JAVA_PROJECT/src/view/OOP_JAVA_PROJECT.java
+++ OOP_JAVA_PROJECT/src/view/OOP_JAVA_PROJECT.java
@@ -30,30 +30,7 @@ public class OOP_JAVA_PROJECT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
-        try { 
-
-            Connection dataBase = new Connection("project", "root", "projetJava2020");
-
-
-
-        } catch (SQLException ex) {
-            Logger.getLogger(OOP_JAVA_PROJECT.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(OOP_JAVA_PROJECT.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
-
-        /*JFrame outerFrame= new JFrame();
-        outerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        outerFrame.setLayout(new BorderLayout());*/
-        
+        //Call of the first and main frame
         MainFrame frame= new MainFrame();
-        
-       /* outerFrame.add(frame, BorderLayout.CENTER);
-        outerFrame.setVisible(true);
-            */
-        //Statistics stat = new Statistics("Member/Customer Ratio");
-
-
     }
 }
